e66387fa7e1193997ff2176067fd4904dbabda14
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-16 15:05:45 +0100

    small changes to seeds and courses index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
 Course.create(
-  title: "some title",
-  description: "some description"
+  title: "Introduction to Ruby and Web development",
+  description: Faker::Lorem.paragraph(10)
+)
+
+Course.create(
+  title: "Rapid Prototyping with Ruby on Rails",
+  description: Faker::Lorem.paragraph(10)
+)
+
+Course.create(
+  title: "Build Robust and Production Quality Applications",
+  description: Faker::Lorem.paragraph(10)
 )
