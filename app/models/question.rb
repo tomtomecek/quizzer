3776fc0b5545675901c94d9f89769a3c34dc5d23
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-18 11:57:12 +0100

    update question_spec with generated_answers method and question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,13 @@
 class Question < ActiveRecord::Base
   belongs_to :quiz
   has_many :answers
+
+  def generated_answers(exam)
+    results = exam.generated_answer_ids.collect do |id|
+      answer = Answer.find(id.to_i)
+      answer if answer.question == self
+    end
+    results.compact
+  end
+
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,4 +3,18 @@ require 'spec_helper'
 describe Question do
   it { is_expected.to belong_to(:quiz) }
   it { is_expected.to have_many(:answers) }
+
+  describe "#generated_answers(exam)" do
+    it "returns an array of multiple generated answers from exam" do
+      quiz      = Fabricate(:quiz)
+      question  = Fabricate(:question, quiz: quiz)
+      a1        = Fabricate(:answer, question: question)
+      a2        = Fabricate(:answer, question: question)
+      question2 = Fabricate(:question, quiz: quiz)
+      a3        = Fabricate(:answer, question: question2)
+      gaids = [a1, a2, a3].map(&:id).map(&:to_s)
+      exam      = Fabricate(:exam, quiz: quiz, generated_answer_ids: gaids)
+      expect(question.generated_answers(exam)).to match_array [a1, a2]
+    end
+  end
 end
