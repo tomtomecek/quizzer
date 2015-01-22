dcc8ce2308d4d16c0281f2187765f380bc2c5796
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-22 21:52:35 +0100

    add current_admin and admin_logged_in? methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :logged_in?, :current_user
+  helper_method :logged_in?, :current_user, :admin_logged_in?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -18,5 +18,13 @@ class ApplicationController < ActionController::Base
       flash[:danger] = "You must be logged in to do that"
       redirect_to root_url
     end
+  end
+
+  def admin_logged_in?
+    !!current_admin
+  end
+
+  def current_admin
+    @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
   end
 end
