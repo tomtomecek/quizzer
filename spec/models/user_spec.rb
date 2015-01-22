0c31f41cc119f2e8c2a0db4ed9e00089b27831c8
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-22 10:21:33 +0100

    cut whitespace

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
   describe ".create_with_omniauth" do
     let(:auth) { OmniAuth.config.mock_auth[:github] }
-    
+
     it "creates a user from omniauth hash" do
       User.create_with_omniauth(auth)
       expect(User.count).to eq(1)
