a2c0630df7804be1555933d39d5c7a605eb53acf
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-22 02:13:44 +0100

    add user model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe User do
+  describe ".create_with_omniauth" do
+    let(:auth) { OmniAuth.config.mock_auth[:github] }
+    
+    it "creates a user from omniauth hash" do
+      User.create_with_omniauth(auth)
+      expect(User.count).to eq(1)
+    end
+  end
+
+  describe ".find_by_provider_and_uid" do
+    let(:auth) { OmniAuth.config.mock_auth[:github] }
+
+    it "finds a user by provider and uid" do
+      Fabricate(:user, provider: "github", uid: "12345")
+      user = User.find_by_provider_and_uid(auth)
+      expect(User.first).to eq user
+    end
+
+    it "does not find a user from omniauth hash" do
+      User.find_by_provider_and_uid(auth)
+      expect(User.first).to be nil
+    end
+  end
+end
