22f726423d285c5f4c90d2fe47218efd521c6ea3
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-22 12:23:17 +0100

    fix styles in header nav, HoundCI feedback3

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do  
-  get '/auth/:provider/callback', to: 'sessions#create'
-  delete 'sign_out', to: 'sessions#destroy'
+  get "/auth/:provider/callback", to: "sessions#create"
+  delete "sign_out", to: "sessions#destroy"
 
   resources :quizzes, except: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resources :exams, only: [:new, :create, :show]
   end
   resources :courses, only: [:show]
-  get 'ui(/:action)', controller: 'ui'
-  root 'courses#index'
+  get "ui(/:action)", controller: "ui"
+  root "courses#index"
 end
