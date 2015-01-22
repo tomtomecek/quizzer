14b679767d60a7f833eb5f18744e2e5acef78cf1
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-23 00:04:59 +0100

    add admin sessions controller

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,14 @@
 class Admin::SessionsController < ApplicationController
-
   def create
-    redirect_to admin_courses_url
+    admin = Admin.find_by(email: params[:email])
+
+    if admin && admin.authenticate(params[:password])
+      session[:admin_id] = admin.id
+      flash[:success] = "Login was successful!"
+      redirect_to admin_courses_url
+    else
+      flash.now[:danger] = "Incorrect email or password. Please try again."
+      render :new
+    end
   end
 end
