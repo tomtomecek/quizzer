73480f0028b8a074e0dd82051616c7db78a25055
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-22 02:13:19 +0100

    config login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do  
+  get '/auth/:provider/callback', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :quizzes, except: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resources :exams, only: [:new, :create, :show]
   end
