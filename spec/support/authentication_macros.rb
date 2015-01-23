811f0e75f0a4b8dae6e7837137faca983ccded1b
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-23 15:09:34 +0100

    add more macros

--- spec/support/authentication_macros.rb
+++ spec/support/authentication_macros.rb
@@ -9,4 +9,14 @@ module AuthenticationMacros
                       username: username)
     visit "/auth/github"
   end
+
+  def sign_in_admin(admin = nil)
+    admin = admin || Fabricate(:admin,
+                               email: "admin@tealeaf.com",
+                               password: "secret")
+    visit admin_sign_in_path
+    fill_in "Email", with: "admin@tealeaf.com"
+    fill_in "Password", with: "secret"
+    click_on "Sign in"
+  end
 end
--- spec/support/expectation_macros.rb
+++ spec/support/expectation_macros.rb
@@ -6,4 +6,8 @@ module ExpectationMacros
   def expect_to_not_see(text)
     expect(page).to have_no_content(text)
   end
+
+  def expect_to_be_in(path)
+    expect(current_path).to eq path
+  end
 end
