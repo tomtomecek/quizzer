fb70df01fbe74be14eac7edab6fe6c1e0c73392a
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-24 23:52:06 +0100

    update fabricators based on validatons

--- spec/fabricators/answer_fabricator.rb
+++ spec/fabricators/answer_fabricator.rb
@@ -1,2 +1,4 @@
 Fabricator(:answer) do
+  content { Faker::Lorem.words(3).join(' ') }
+  correct { [true, false].sample }
 end
--- spec/fabricators/question_fabricator.rb
+++ spec/fabricators/question_fabricator.rb
@@ -1,2 +1,4 @@
 Fabricator(:question) do
+  content { Faker::Lorem.words(2).join(' ') }
+  points { (1..5).to_a.sample }
 end
