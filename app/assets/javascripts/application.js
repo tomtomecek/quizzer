be6b52646ea7c0239fad42de10bfce06d8415437
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-26 17:20:04 +0100

    working add Question and add Answer code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,20 @@
 //= require_tree .
 
 $(document).ready(function() {
-  $("[data-toggle='popover']").popover('show'); 
+  $('[data-toggle=popover]').popover('show');
+
+  $('form').on('click', '.remove_fields', function(e) {
+    $(this).prev('input[type=hidden]').val('1');
+    $(this).closest('fieldset').remove();
+    e.preventDefault();  
+  });
+
+  $('form').on('click', '.add_fields', function(e) {
+    var id = $(this).data('id');
+    var time = new Date().getTime();
+    var regexp = new RegExp(id, 'g');
+    var inserter = $(this).closest('.form-group').prev('.inserter');
+    inserter.before($(this).data('fields').replace(regexp, time));
+    e.preventDefault();
+  });
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def github_sign_in_path
     "/auth/github"
   end
+
+  def link_to_add_fields(name, f, association, options = {})
+    new_object = f.object.send(association).build
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render "#{association.to_s.singularize}_fields", f: builder
+    end
+    plus_icon = content_tag(:span, '', class: "glyphicon glyphicon-plus-sign")
+    link_to '#', class: "add_fields #{options[:class]}", data: { id: id, fields: fields.gsub("\n", "") } do
+       plus_icon + " " + name
+    end    
+  end
 end
