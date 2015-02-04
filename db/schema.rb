f05633c1d27c01dc5ee2b4f6454490feebdd83d1
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-04 15:50:35 +0100

    add timestamps to questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124102002) do
+ActiveRecord::Schema.define(version: 20150204142628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,9 +51,11 @@ ActiveRecord::Schema.define(version: 20150124102002) do
   add_index "exams", ["student_id"], name: "index_exams_on_student_id", using: :btree
 
   create_table "questions", force: true do |t|
-    t.integer "quiz_id"
-    t.text    "content"
-    t.integer "points"
+    t.integer  "quiz_id"
+    t.text     "content"
+    t.integer  "points"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "questions", ["quiz_id"], name: "index_questions_on_quiz_id", using: :btree
