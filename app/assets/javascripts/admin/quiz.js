a9319aca339d17ac059cb7fc140ca5413f47893d
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-05 00:51:18 +0100

    update quiz.js - moved _destroy field up from fieldset

--- app/assets/javascripts/admin/quiz.js
+++ app/assets/javascripts/admin/quiz.js
@@ -2,8 +2,9 @@ ready = function() {
   $('[data-toggle=popover]').popover('show');
 
   $('form').on('click', '.remove_fields', function(e) {
-    $(this).prev('input[type=hidden]').val('1');
-    $(this).closest('fieldset').fadeOut(function() {
+    var fieldset = $(this).closest('fieldset');
+    fieldset.prev('input[type=hidden]').val('1');
+    fieldset.fadeOut(function() {
       $(this).remove();
     });
     e.preventDefault();
@@ -11,9 +12,9 @@ ready = function() {
 
   $('form').on('click', '.add_fields', function(e) {
     $('form').parsley().destroy();
-    var id = $(this).data('id');
-    var time = new Date().getTime();
-    var regexp = new RegExp(id, 'g');
+    var id       = $(this).data('id');
+    var time     = new Date().getTime();
+    var regexp   = new RegExp(id, 'g');
     var inserter = $(this).closest('.form-group').prev('.inserter');
     inserter.before($(this).data('fields').replace(regexp, time));
     $('form').parsley();
