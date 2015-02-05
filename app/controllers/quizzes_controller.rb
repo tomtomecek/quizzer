51aafcd84307cd0253ec35bf50de0956909cf60e
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-05 01:01:09 +0100

    add edit, update, show controller code plus rescue_from refactor, add routes

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,30 +1,47 @@
 class QuizzesController < AdminController
+  before_action :find_quiz, only: [:show, :edit, :update]
+  rescue_from ActiveRecord::NestedAttributes::TooManyRecords, with: :too_many_records
+
   def new
     course = Course.find_by(slug: params[:course_id])
     @quiz = course.quizzes.build
   end
 
   def create
-    course = Course.find_by(slug: params[:course_id])
+    @course = Course.find_by(slug: params[:course_id])
 
-    @quiz = course.quizzes.build(quiz_params)
+    @quiz = @course.quizzes.build(quiz_params)
 
     if @quiz.save
       flash[:success] = "Successfully created new quiz."
-      redirect_to admin_course_url(course)
+      redirect_to admin_course_url(@course)
     else
       flash.now[:danger] = "Quiz creation failed - check errors below:"
       render :new
     end
+  end
 
-  rescue ActiveRecord::NestedAttributes::TooManyRecords => e
-    downsize_params
-    @quiz = course.quizzes.build(quiz_params)
-    flash.now[:info] = "Quiz creation failed - #{e.message}"
-    render :new
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @quiz.update(quiz_params)
+      flash[:success] = "Quiz was successfully updated."
+      redirect_to admin_course_path(@quiz.course)
+    else
+      flash.now[:danger] = "Quiz creation failed - check errors below:"
+      render :edit
+    end
   end
 
 private
+
+  def find_quiz
+    @quiz = Quiz.find_by(slug: params[:id])
+  end
 
   def quiz_params
     params.require(:quiz).permit(
@@ -32,10 +49,12 @@ private
       :description,
       :published,
       questions_attributes: [
+        :id,
         :content,
         :points,
         :_destroy,
         answers_attributes: [
+          :id,
           :content,
           :correct,
           :_destroy
@@ -49,6 +68,18 @@ private
       until q_attrs[1][:answers_attributes].count <= 10
         q_attrs[1][:answers_attributes].shift
       end
+    end
+  end
+
+  def too_many_records(error)
+    downsize_params
+    flash.now[:info] = "Quiz creation failed - #{error.message}"
+
+    if @quiz.nil?
+      @quiz = @course.quizzes.build(quiz_params)
+      render :new
+    else
+      render :edit
     end
   end
 end
