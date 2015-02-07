4a8761ada21d7189870378e271929eb0f8502e08
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-07 16:12:38 +0100

    add new shared example on redirect users

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -24,3 +24,12 @@ shared_examples "require admin sign out" do
     expect(response).to redirect_to admin_courses_url
   end
 end
+
+shared_examples "redirect users" do
+  it "redirects signed in users to root url" do
+    set_current_user
+    action
+    expect(flash[:info]).to be_present
+    expect(response).to redirect_to root_url
+  end
+end
