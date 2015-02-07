fb3dda9952e6ac33e99e07b64140a54b0c547ffb
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-07 01:50:43 +0100

    add patch update for exam as submission

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -17,6 +17,23 @@ class ExamsController < ApplicationController
   end
 
   def update
-    
+    @quiz = Quiz.find_by(slug: params[:quiz_id])
+    @exam = current_user.exams.find(params[:id])
+    student_answers = @exam.generated_answers.
+                        where(id: params[:student_answer_ids])
+    if answers_valid?(student_answers)
+      student_answers.each { |a| a.update_column(:student_marked, true) }
+      flash[:success] = "xxx"
+      redirect_to [@quiz, @exam]
+    else
+      flash[:danger] = "Failed"
+      render :new
+    end
+  end
+
+private
+
+  def answers_valid?(student_answers)
+    student_answers.count == params[:student_answer_ids].count
   end
 end
