7f03612533593ec2b4f7006a19e9f2d742c52352
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-07 16:17:47 +0100

    add password resets controller code and generate token to admin model

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,25 @@
 class PasswordResetsController < ApplicationController
+  before_action :require_sign_out, only: [:new, :create]
+
+  def new
+  end
+
   def create
+    admin = Admin.find_by(email: params[:email])
+    if admin
+      admin.generate_password_reset_token
+      AdminMailer.send_reset_token(admin).deliver
+    end
     redirect_to confirm_password_reset_url
+  end
+
+private
+
+  def require_sign_out
+    if current_admin || current_user
+      flash[:info] = "This is not available."
+      redirect_to root_url if current_user
+      redirect_to admin_courses_url if current_admin
+    end
   end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,11 @@
 class Admin < ActiveRecord::Base
   has_secure_password validations: false
+
+  def generate_password_reset_token
+    begin
+      token = SecureRandom.urlsafe_base64
+    end while Admin.exists?(password_reset_token: token)
+
+    update_column(:password_reset_token, token)
+  end
 end
