c119205014c14eae46f0857f37ba8ef5e5ea9a3a
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-08 10:17:08 +0100

    add edit and update password resets controller code

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,5 @@
 class PasswordResetsController < ApplicationController
-  before_action :require_sign_out, only: [:new, :create]
+  before_action :require_sign_out, only: [:new, :create, :edit, :update]
 
   def new
   end
@@ -7,10 +7,33 @@ class PasswordResetsController < ApplicationController
   def create
     admin = Admin.find_by(email: params[:email])
     if admin
-      admin.generate_password_reset_token
+      admin.generate_password_reset_items
       AdminMailer.send_reset_token(admin).deliver
     end
     redirect_to confirm_password_reset_url
+  end
+
+  def edit
+    @admin = Admin.find_by(password_reset_token: params[:token])
+    if @admin && (@admin.password_reset_expires_at < Time.zone.now)
+      redirect_to expired_token_url
+    elsif @admin.nil?
+      redirect_to root_url
+    end
+  end
+
+  def update
+    @admin = Admin.find_by(password_reset_token: params[:token])
+    if @admin && @admin.update(admin_params)
+      @admin.clear_token_and_expires_at!
+      flash[:success] = "You can sign in with your new password now"
+      redirect_to admin_sign_in_url
+    elsif @admin.nil?
+      flash[:danger] = "Please, retry reseting your password again."
+      redirect_to root_url
+    else
+      render :edit
+    end
   end
 
 private
@@ -21,5 +44,9 @@ private
       redirect_to root_url if current_user
       redirect_to admin_courses_url if current_admin
     end
+  end
+
+  def admin_params
+    params.require(:admin).permit(:password)
   end
 end
