83146499fe9dadae0427d86d60fd6825ecc3e363
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-10 11:13:27 +0100

    add feature spec admin checks exams

---
+++ spec/features/admin_checks_exams_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature "admin checks on exams" do
+  scenario "admin finds exams and goes through the list" do
+    alice = Fabricate(:user)
+    jake = Fabricate(:user)
+    ruby = Fabricate(:course)
+    rails = Fabricate(:course)
+    week1_ruby = Fabricate(:quiz, course: ruby)
+    week1_rails = Fabricate(:quiz, course: rails)
+    exam1 = Fabricate(:exam, student: alice, quiz: week1_ruby)
+    exam2 = Fabricate(:exam, student: jake, quiz: week1_rails)
+
+    visit admin_exams_path
+    expect_to_be_in admin_exams_path
+
+    within(:css, "tr#exam_#{exam1.id}") do
+      expect_to_see alice.username
+      expect_to_see week1_ruby.title
+      expect_to_see ruby.title
+      click_on "View"
+    end
+
+    expect_to_be_in admin_exam_path(exam1)
+  end
+end
