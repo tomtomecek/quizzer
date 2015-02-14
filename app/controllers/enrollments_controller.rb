bde0f77aa3a887ff7e6f37ac3bb0de1dba36c815
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-14 16:17:06 +0100

    finish enrollments controller XHR POST create with mocks

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -10,15 +10,44 @@ class EnrollmentsController < ApplicationController
     course = Course.find(params[:enrollment][:course_id])
     @enrollment = current_user.enrollments.build(enrollment_params)
 
-    if @enrollment.save
-      flash.keep[:success] = "You have now enrolled course #{course.title}"
-      render js: "window.location.replace('#{course_url(course)}');"
+    if @enrollment.valid?
+      if enrollment_paid?
+        charge = StripeWrapper::Charge.create(
+          amount: 1999,
+          card_token: params[:stripeToken]
+        )
+        if charge.successful?
+          @enrollment.save
+          flash.keep[:success] = "You have now enrolled course #{course.title}"
+          render js: "window.location.replace('#{course_url(course)}');"
+        else
+          @error = charge.error_message
+          flash.now[:danger] = @error
+          render :new
+        end
+      elsif enrollment_free?
+        @enrollment.save
+        flash.keep[:success] = "You have now enrolled course #{course.title}"
+        render js: "window.location.replace('#{course_url(course)}');"
+      else
+        @error = "Invalid operation."
+        flash.now[:danger] = @error
+        render :new
+      end
     else
       render :new
     end
   end
 
 private
+
+  def enrollment_paid?
+    params[:enrollment][:paid] == "1" && params[:stripeToken].present?
+  end
+
+  def enrollment_free?
+    params[:enrollment][:paid] == "0"  && params[:stripeToken].nil?
+  end
 
   def enrollment_params
     params.require(:enrollment).permit(:course_id, :paid, :honor_code)
--- spec/controllers/enrollments_controller_spec.rb
+++ spec/controllers/enrollments_controller_spec.rb
@@ -27,65 +27,194 @@ describe EnrollmentsController do
       let(:action) { xhr :post, :create, enrollment: { course_id: 1 } }
     end
 
-    context "with valid inputs" do
-      before do
-        xhr :post,
-            :create,
-            enrollment: {
-              course_id: ruby.id,
-              honor_code: "1",
-              paid: "0"
-            }
+    context "Free enrollment" do
+      context "with valid data" do
+        before do
+          expect(StripeWrapper::Charge).to_not receive(:create)
+          xhr :post, :create, enrollment: {
+            course_id: ruby.id,
+            honor_code: "1",
+            paid: "0"
+          }
+        end
+
+        it "redirects to course show" do
+          ajax_redirect = "window.location.replace('#{course_url(ruby)}');"
+          expect(response.body).to include(ajax_redirect)
+        end
+
+        it "sets the flash success" do
+          expect(flash.keep[:success]).to be_present
+        end
+
+        it "creates an enrollment" do
+          expect(Enrollment.count).to eq 1
+        end
+
+        it "creates paid enrollment" do
+          expect(Enrollment.first).to_not be_paid
+        end
+
+        it "creates an enrollment under course" do
+          expect(ruby.reload.enrollments).to include Enrollment.first
+        end
+
+        it "creates an enrollment under student" do
+          expect(current_user.enrollments).to include Enrollment.first
+        end
+
+        it "doesn't charge the card" do
+          expect(StripeWrapper::Charge).to_not receive(:create)
+          xhr :post, :create, enrollment: {
+            course_id: ruby.id,
+            honor_code: "1",
+            paid: "0"
+          }
+        end
       end
 
-      it "redirects to course show" do
-        ajax_redirect = "window.location.replace('#{course_url(ruby)}');"
-        expect(response.body).to include(ajax_redirect)
-      end
+      context "with invalid inputs" do
+        before do
+          xhr :post, :create, enrollment: {
+            course_id: ruby.id,
+            honor_code: "0",
+            paid: "0"
+          }
+        end
 
-      it "sets the flash success" do
-        expect(flash.keep[:success]).to be_present
-      end
+        it "renders the :new template" do
+          expect(response).to render_template :new
+        end
 
-      it "creates an enrollment" do
-        expect(Enrollment.count).to eq 1
-      end
+        it "does not create an enrollment" do
+          expect(Enrollment.count).to eq 0
+        end
 
-      it "creates an enrollment under course" do
-        expect(ruby.reload.enrollments).to include Enrollment.first
-      end
+        it "sets the @enrollment" do
+          expect(assigns(:enrollment)).to be_new_record
+          expect(assigns(:enrollment)).to be_instance_of Enrollment
+        end
 
-      it "creates an enrollment under student" do
-        expect(current_user.enrollments).to include Enrollment.first
+        it "doesn't charge the card" do
+          expect(StripeWrapper::Charge).to_not receive(:create)
+          xhr :post, :create, enrollment: {
+            course_id: ruby.id,
+            honor_code: "0",
+            paid: "0"
+          }
+        end
       end
     end
 
-    context "with invalid inputs" do
-      before do
-        xhr :post,
-            :create,
-            enrollment: {
-              course_id: ruby.id,
-              honor_code: "0",
-              paid: "0"
-            }
+    context "Paid enrollment" do
+      context "with valid enrollment and valid card" do
+        let(:charge) { double('charge', successful?: true) }
+        before do
+          expect(StripeWrapper::Charge).to receive(:create).and_return(charge)
+          xhr :post, :create, stripeToken: "123123", enrollment: {
+            course_id: ruby.id,
+            honor_code: "1",
+            paid: "1"
+          }
+        end
+
+        it "redirects to course show" do
+          ajax_redirect = "window.location.replace('#{course_url(ruby)}');"
+          expect(response.body).to include(ajax_redirect)
+        end
+
+        it "sets the flash success" do
+          expect(flash.keep[:success]).to be_present
+        end
+
+        it "creates enrollment" do
+          expect(Enrollment.count).to eq 1
+        end
+
+        it "creates paid enrollment" do
+          expect(Enrollment.first).to be_paid
+        end
       end
 
-      it "renders the :new template" do
+      context "with valid enrollment and declined card" do
+        let(:charge) do
+          double('charge',
+                 successful?: false,
+                 error_message: "Your card was declined")
+        end
+        before do
+          expect(StripeWrapper::Charge).to receive(:create).and_return(charge)
+          xhr :post, :create, stripeToken: "345345", enrollment: {
+            course_id: ruby.id,
+            honor_code: "1",
+            paid: "1"
+          }
+        end
+
+        it "renders the :new template" do
+          expect(response).to render_template :new
+        end
+
+        it "sets the flash danger" do
+          expect(flash.now[:danger]).to be_present
+        end
+
+        it "does not create enrollment" do
+          expect(Enrollment.count).to eq 0
+        end
+
+      end
+
+      context "with invalid enrollment" do
+        it "does not create charge" do
+          expect(StripeWrapper::Charge).to_not receive(:create)
+          xhr :post, :create, stripeToken: "345345", enrollment: {
+            course_id: ruby.id,
+            honor_code: "0",
+            paid: "1"
+          }
+        end
+
+        it "renders the :new tempalte" do
+          xhr :post, :create, stripeToken: "345345", enrollment: {
+            course_id: ruby.id,
+            honor_code: "0",
+            paid: "1"
+          }
+          expect(response).to render_template :new
+        end
+      end
+    end
+
+    context "valid enrollment - invalid payment params" do
+      it "renders the :new template for paid param hack" do
+        expect(StripeWrapper::Charge).to_not receive(:create)
+        xhr :post, :create, stripeToken: "123123", enrollment: {
+          course_id: ruby.id,
+          honor_code: "1",
+          paid: "no match"
+        }
         expect(response).to render_template :new
       end
 
-      it "does not create an enrollment" do
-        expect(Enrollment.count).to eq 0
+      it "renders the :new template for stripe token missing" do
+        expect(StripeWrapper::Charge).to_not receive(:create)
+        xhr :post, :create, enrollment: {
+          course_id: ruby.id,
+          honor_code: "1",
+          paid: "1"
+        }
+        expect(response).to render_template :new
       end
 
-      it "sets the @enrollment" do
-        expect(assigns(:enrollment)).to be_new_record
-        expect(assigns(:enrollment)).to be_instance_of Enrollment
-      end
-
-      it "sets the errors" do
-        expect(assigns(:enrollment).errors.any?).to be true
+      it "sets the flash danger" do
+        expect(StripeWrapper::Charge).to_not receive(:create)
+        xhr :post, :create, enrollment: {
+          course_id: ruby.id,
+          honor_code: "1",
+          paid: "1"
+        }
+        expect(flash[:danger]).to be_present
       end
     end
   end
