e0f512d8b9c10259de5859193b9a45f463193125
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-15 22:23:40 +0100

    update quiz fabricator

--- spec/fabricators/quiz_fabricator.rb
+++ spec/fabricators/quiz_fabricator.rb
@@ -1,6 +1,7 @@
 Fabricator(:quiz) do
   title { Faker::Lorem.words(3).join(' ') }
   description { Faker::Lorem.paragraph }
+  passing_percentage { 0.step(100, 5).to_a.sample }
   course
   questions(count: 1)
 end
