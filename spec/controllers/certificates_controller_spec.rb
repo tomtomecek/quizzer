624e4c19beade198c9bffca4507ee6a7975b9556
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-16 18:05:49 +0100

    add certificates controller specs and code, #add_licence_number

---
+++ spec/controllers/certificates_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe CertificatesController do
+  let(:ruby) { Fabricate(:course) }
+  before do
+    set_current_user
+    set_enrollment(current_user, ruby)
+  end
+
+  describe "POST create" do
+    it_behaves_like "require sign in" do
+      let(:action) { post :create, enrollment_id: 1 }
+    end
+
+    it_behaves_like "requires enrollment" do
+      let(:action) { post :create, enrollment_id: 1 }
+    end
+
+    context "with valid data" do
+      let(:paid) { Fabricate(:enrollment, paid: true, student: current_user) }
+
+      before { post :create, enrollment_id: paid.id }
+
+      it { is_expected.to redirect_to Certificate.first }
+
+      it "creates certificate" do
+        expect(Certificate.count).to eq 1
+      end
+
+      it "creates certificate under enrollment" do
+        expect(paid.certificate).to eq Certificate.first
+      end
+
+      it "creates certificate under student" do
+        expect(current_user.certificates.first).to eq Certificate.first
+      end
+    end
+
+    context "with invalid data" do
+      let(:free) { Fabricate(:enrollment, paid: false, student: current_user) }
+      before { post :create, enrollment_id: free.id }
+
+      it { is_expected.to redirect_to :root }
+      it { is_expected.to set_the_flash[:danger] }
+      it "does not create certificate" do
+        expect(Certificate.count).to eq 0
+      end
+    end
+  end
+
+  describe "GET show" do
+    let(:cert) { Fabricate(:certificate, student: current_user) }
+    it_behaves_like "require sign in" do
+      let(:action) { get :show, licence_number: "12313" }
+    end
+  end
+end
--- spec/models/certificate_spec.rb
+++ spec/models/certificate_spec.rb
@@ -3,4 +3,16 @@ require 'spec_helper'
 describe Certificate do
   it { is_expected.to belong_to(:enrollment) }
   it { is_expected.to belong_to(:student).class_name("User") }
+
+  describe "#generate licence number" do
+    it "creates certificate with licence number" do
+      cert = Fabricate(:certificate)
+      expect(cert.licence_number).to_not be nil
+    end
+
+    it "creates certificate with uniq licence number" do
+      certifications = Fabricate.times(100, :certificate)
+      expect(certifications.map(&:licence_number).uniq.size).to eq 100
+    end
+  end
 end
