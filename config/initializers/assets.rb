4e9760570e3d1f57940a8a4b46c6436b0ae7ddad
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-18 21:09:22 +0100

    heroku test asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(admin/quiz.js enroll.js certificate-pdf.css.sass)
+Rails.application.config.assets.precompile += %w(admin/quiz.js enroll.js certificate-pdf.css)
