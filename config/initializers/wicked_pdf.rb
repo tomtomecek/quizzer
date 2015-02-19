64c6f979d3a6b5b91459574141d3e64fe533fdec
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-19 08:27:27 +0100

    manually add binary for wkhtmltopdf for heroku linux

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,11 @@
-WickedPdf.config = {
-  exe_path: '/usr/local/bin/wkhtmltopdf'
-}
+WickedPdf.config do |config|  
+  if Rails.env.production? || Rails.env.staging? then
+    config.exe_path = Rails.root.to_s + '/bin/wkhtmltopdf'
+  else  
+    if /darwin/ =~ RUBY_PLATFORM then
+      config.exe_path = '/usr/local/bin/wkhtmltopdf'
+    else
+      raise "UnableToLocateWkhtmltopdf"
+    end
+  end
+end
