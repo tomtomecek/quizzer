eac46891607768abcd86974d6a68b1b8aaee1fee
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-20 00:24:39 +0100

    change PDFs structure to A4

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -6,7 +6,7 @@ class CertificatesController < ApplicationController
     enrollment = Enrollment.find(params[:enrollment_id])
     if enrollment.paid?
       certificate = current_user.certificates.create(enrollment: enrollment)
-      redirect_to certificate_url(certificate.licence_number)
+      redirect_to certificate_url(certificate.licence_number, format: "pdf")
     else
       flash[:danger] = "Free enrollments are not enligible for certificates."
       redirect_to root_url
@@ -15,15 +15,12 @@ class CertificatesController < ApplicationController
 
   def show
     @certificate = Certificate.find_by(licence_number: params[:licence_number])
-
     respond_to do |format|
-      format.html { render layout: 'layouts/application.pdf.haml' }
+      format.html
       format.pdf do
         render pdf:           'certificate_test',
                layout:        'layouts/application.pdf.haml',
-               show_as_html:  params[:debug].present?,
-               orientation:   'Landscape',
-               page_size:     'Letter'
+               show_as_html:  params[:debug].present?
       end
     end
   end
