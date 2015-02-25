c9624757aa434f527a50cf34cc992bba20df8ab5
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-25 15:44:31 +0100

    update/refactor instructor creates admin feature spec

--- spec/features/instructor_creates_admin_spec.rb
+++ spec/features/instructor_creates_admin_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 feature "instructor creates an admin account" do
+  given(:kevin) { Fabricate(:instructor) }
+  background do
+    sign_in_admin(kevin)
+  end
   after { clear_emails }
 
   scenario "instructor creates teaching assistent" do
-    kevin = Fabricate(:instructor)
-    sign_in_admin(kevin)
     click_on "Admin Management"
     expect_to_be_in management_admins_path
     click_on "Add new admin"
@@ -13,20 +15,17 @@ feature "instructor creates an admin account" do
     fill_in "Email:", with: "brandon@email.com"
     select "Teaching Assistant", from: "Admin role:"
     click_on "Add new admin"
-    # save_and_open_page
     expect_to_see "New admin account was created, email was sent with\
 instructions"
   end
 
   scenario "instructor creates instructor" do
-    kevin = Fabricate(:admin, role: "instructor")
-    sign_in_admin(kevin)
     click_on "Admin management"
-    expect_to_be_in admins_path
+    expect_to_be_in management_admins_path
     click_on "Add new admin"
     expect_to_be_in new_admin_path
-    fill_in "email", with: "chris@email.com"
-    check "Instructor"
+    fill_in "Email:", with: "chris@email.com"
+    select "Instructor", from: "Admin role:"
     click_on "Add new admin"
     expect_to_see "New admin account was created, email was sent with\
 instructions"
