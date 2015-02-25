ef9a4b732df675ae28db735da623398b2bdc6a1a
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-25 16:09:54 +0100

    role updates

--- spec/features/instructor_creates_admin_spec.rb
+++ spec/features/instructor_creates_admin_spec.rb
@@ -13,7 +13,7 @@ feature "instructor creates an admin account" do
     click_on "Add new admin"
     expect_to_be_in new_admin_path
     fill_in "Email:", with: "brandon@email.com"
-    select "Teaching Assistant", from: "Admin role:"
+    select "Teaching assistant", from: "Admin role:"
     click_on "Add new admin"
     expect_to_see "New admin account was created, email was sent with\
 instructions"
