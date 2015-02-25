c041ef037eec6cd65dbb00ed8c0ad19442fb967b
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-25 14:37:42 +0100

    add activated column to admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225113550) do
+ActiveRecord::Schema.define(version: 20150225133633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150225113550) do
     t.string   "password_reset_token"
     t.datetime "password_reset_expires_at"
     t.string   "remember_digest"
+    t.boolean  "activated",                 default: false
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
