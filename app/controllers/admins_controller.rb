9e38c3dcd660ce5de56395109250eda9a17f3059
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-25 15:04:48 +0100

    add require instructor to admins controller and admin mailer

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,6 @@
 class AdminsController < AdminController
+  before_action :require_instructor, only: [:new, :create]
+
   def index
   end
 
@@ -8,11 +10,24 @@ class AdminsController < AdminController
 
   def create
     @admin = Admin.new(admin_params)
+    if @admin.save
+      AdminMailer.delay.send_activation_link(@admin)
+      redirect_to management_admins_path
+    else
+
+    end
   end
 
 private
 
   def admin_params
     params.require(:admin).permit(:email, :role)
+  end
+
+  def require_instructor
+    unless current_admin.instructor?
+      flash[:danger] = "You are not Instructor"
+      redirect_to admin_courses_url
+    end
   end
 end
