d0dcfee7aacf0b6a892e6912e76d0970726f693e
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-25 12:23:54 +0100

    update admin fabricator

--- spec/fabricators/admin_fabricator.rb
+++ spec/fabricators/admin_fabricator.rb
@@ -1,3 +1,9 @@
 Fabricator(:admin) do
+  email { Faker::Internet.email }
   password { "password" }
+  role { "teaching assistant" }
+end
+
+Fabricator(:instructor, from: :admin) do
+  role { "instructor" }
 end
