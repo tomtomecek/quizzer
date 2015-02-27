71e36fa4a0f5f0e9604088ced291eb766753cbd2
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-27 20:48:50 +0100

    refactor instructor creates course spec a bit

--- spec/features/instructor_creates_course_spec.rb
+++ spec/features/instructor_creates_course_spec.rb
@@ -4,14 +4,14 @@ feature "instructor creates a new course" do
   given(:kevin) { Fabricate(:instructor, username: "Kevin", activated: true) }
   background { sign_in_admin kevin }
 
-  scenario "standard course creation" do
+  scenario "standard course creation", :js do
     navigate_to_new_course
     fill_in "Title", with: "HTML and CSS basics"
     fill_in "Description", with: "front end course"
     fill_in "Duration", with: "4 weeks"
     select "Kevin", from: "Instructors"
     fill_in "Quizzes to pass", with: "4"
-    attach_file 'Image', 'spec/support/images/ruby_on_rails.jpg'
+    # attach_file 'Image', 'spec/support/images/ruby_on_rails.jpg'
 
     click_on "Create Course"
     expect_to_see "Unpublished course has been created."
@@ -19,24 +19,27 @@ feature "instructor creates a new course" do
 
   scenario "check error messages", :js do
     navigate_to_new_course
-    fill_in "Title", with: ""
-    expect_to_see "This value is required."
-
-    fill_in "Description", with: ""
-    expect_to_see "This value is required."
-
-    fill_in "Duration", with: ""
-    expect_to_see "This value is required."
+    validate_required_error_message_for("Title")
+    validate_required_error_message_for("Description")
+    validate_required_error_message_for("Duration")
 
     select "Select Instructors", from: "Instructors"
-    expect_to_see "Course must have at least 1 instructor."
 
-    select "Select Minimum", from: "Quizzes to pass"
-    expect_to_see "You must select minimum amount of quizzes to pass."
+    fill_in "Quizzes to pass", with: ""
+    expect_to_see "Set minimum amount of quizzes to pass."
+
+    click_on "Create Course"
+    expect_to_see "Course must have at least 1 instructor."
   end
 end
 
 def navigate_to_new_course
   click_on "Courses"
   click_on "New Course"
+end
+
+def validate_required_error_message_for(label)
+  fill_in label, with: ""
+  expect_to_see "This value is required."
+  fill_in label, with: "something"
 end
