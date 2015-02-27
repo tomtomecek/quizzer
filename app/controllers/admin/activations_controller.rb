f848b7a1faab7d0a891d6b49b82be00165cbe684
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-27 15:54:43 +0100

    change admin activation to form_for and update controller

--- app/controllers/admin/activations_controller.rb
+++ app/controllers/admin/activations_controller.rb
@@ -4,19 +4,22 @@ class Admin::ActivationsController < ApplicationController
   def new; end
 
   def create
-    if @admin.update(password: params[:password])
+    if @admin.update(admin_params)
       @admin.update_columns(activated: true, activation_token: nil)
       session[:admin_id] = @admin.id
       flash[:success] = "Welcome to Tealeaf! You are new admin."
       redirect_to admin_courses_url
     else
-      flash.now[:danger] = "Invalid Password.\
- It must have at least 6 characters."
+      flash.now[:danger] = "These errors need a fix:"
       render :new
     end
   end
 
 private
+
+  def admin_params
+    params.require(:admin).permit(:username, :password)
+  end
 
   def require_token
     @admin = Admin.find_by(activation_token: params[:activation_token])
