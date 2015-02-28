ed076dbb1a86d4018e066bac5629482c23a45a72
tomtomecek <tomas.tomecek@hotmail.com>
2015-02-28 01:34:38 +0100

    add image uploader, adjust feature spec

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,17 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  def store_dir
+    Rails.env.test? ? '/tmp/test' : '/tmp/development'
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  process :resize_to_fit => [667, 320]
+
+  version :certificate do
+    process :resize_to_fill => [460, 322]
+  end
+end
