887036e1e409bcbaad84eba298ce34cc0c2116c4
tomtomecek <tomas.tomecek@hotmail.com>
2015-03-01 01:27:44 +0100

    uploader changes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,16 +2,22 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   def store_dir
-    Rails.env.test? ? '/tmp/test' : '/tmp/development'
+    if Rails.env.test?
+      'tmp'
+    elsif Rails.env.development?
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    else
+      'public/uploads'
+    end
   end
 
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
-  process resize_to_fill: [667, 320]
+  process resize_to_fit: [667, 320]
 
   version :certificate do
-    process resize_to_fill: [460, 322]
+    process resize_to_fit: [460, 322]
   end
 end
