4f3d54aa245453df558000b4fa6050f93aacc8d7
tomtomecek <tomas.tomecek@hotmail.com>
2015-03-02 19:12:41 +0100

    add form and finish instructor edits course

--- spec/features/instructor_edits_course_spec.rb
+++ spec/features/instructor_edits_course_spec.rb
@@ -1,29 +1,45 @@
 require 'spec_helper'
 
 feature "instructor edits course" do
-  given(:kevin) { Fabricate(:instructor, username: "Kevin") }
+  given(:kevin) do
+    Fabricate(:instructor,
+              username: "Kevin",
+              full_name: "Kevin Wang",
+              activated: true)
+  end
+
   given!(:ruby) { Fabricate(:course) }
   background { sign_in_admin(kevin) }
 
   scenario "successfull edit" do
-    visit edit_admin_course_path(ruby.slug)
+    click_on "Courses"
+    click_on "Edit"
     expect_to_see "Edit course #{ruby.title}"
 
     fill_in "Title", with: "HTML and CSS basics"
     fill_in "Description", with: "front end course"
-    fill_in "Duration", with: "4 weeks"
+    fill_in "Duration", with: "7 weeks"
     select "Kevin", from: "Instructor"
     fill_in "Quizzes to pass", with: "3"
     fill_in "Certificate (in $)", with: "419.99"
     attach_file 'Image', 'spec/support/images/ruby.jpg'
 
     click_on "Submit changes"
+    expect_to_see "Successfully updated the course HTML and CSS basics"
+    click_on "Quizzer"
 
-    within_nav { click_on "Quizzer" }
-    expect(page).to have_css("img[src='#{ruby.image_cover_url}']", visible: true)
+    validate_course_update(ruby)
   end
 
   scenario "fail edit - error messages" do
     visit edit_admin_course_path(ruby.slug)
   end
+end
+
+def validate_course_update(ruby)
+  expect_to_see "Price $419.99"
+  expect_to_see "7 weeks"
+  expect_to_see "Kevin Wang"
+  expect_to_see "front end course"
+  expect(page).to have_css("img[src='#{ruby.reload.image_cover_url}']", visible: true)
 end
