774e34d670488887c3b3b39bbfb09c625c5ca605
tomtomecek <tomas.tomecek@hotmail.com>
2015-03-02 10:38:29 +0100

    update tealeaf presentation logic

--- app/assets/javascripts/jumbotron.js
+++ app/assets/javascripts/jumbotron.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
-  $('.ta-subtitle em').delay('3000').fadeIn('slow');
+$(document).ready(function() {
+  var t = 4000;
+
+  $('.ta-leaf em').delay('2000').fadeIn(3000);
+  $('.ta-leaf ul > li:nth-child(1n)').each(function() {
+    t += 2000;
+    return $(this).delay(t).fadeIn(2000);
+  });
 });
