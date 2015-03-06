2901c2ad0a7e0476a0ff45ab4c567fdacffea44d
tomtomecek <tomas.tomecek@hotmail.com>
2015-03-06 16:05:35 +0100

    increase sleep before email opening

--- spec/features/admin_activates_account_spec.rb
+++ spec/features/admin_activates_account_spec.rb
@@ -14,7 +14,7 @@ feature "admin activates account" do
   after { clear_emails }
 
   scenario "receives email and activates the account", :js do
-    sleep 0.1
+    sleep 2
     open_email(email)
     current_email.click_on activation_path
 
@@ -24,7 +24,7 @@ feature "admin activates account" do
 
   context "checks errors" do
     background do
-      sleep 0.1
+      sleep 2
       open_email(email)
       current_email.click_link activation_path
     end
