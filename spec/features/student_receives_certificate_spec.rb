72f239276dd425ec757250f03642e0ca52c1ab16
tomtomecek <tomas.tomecek@hotmail.com>
2015-03-06 16:23:40 +0100

    correctly configured

--- spec/features/student_receives_certificate_spec.rb
+++ spec/features/student_receives_certificate_spec.rb
@@ -35,7 +35,6 @@ feature "student receives certificate" do
     click_on "Submit Answers"
     expect_to_see course_completion_message
 
-    sleep 2
     open_email("alice@example.com")
     expect_to_see_in_email "Please access your Certificate of Accomplishment"
     current_email.click_link "here"
